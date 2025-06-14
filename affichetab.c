94231c2c9da680c46fda45fc9c4b36f23c63c496
razaye <angezabakani@gmail.com>
2025-06-14 15:55:23 +0200

    Update affichetab.c

--- affichetab.c
+++ affichetab.c
@@ -1,1 +1,11 @@
 //affichetab by ange
+int tabA[10];
+int i;
+
+int chx;
+void affiche_tab(int tab[], int taille) {
+    int i;
+    for(i = 0; i < taille; i++) {
+        printf("%d ", tab[i]);
+    }
+    printf("\n");
